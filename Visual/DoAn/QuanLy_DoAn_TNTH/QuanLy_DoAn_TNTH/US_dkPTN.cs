89ee0f60aa9593f7a87acca960456ac89fcdbeb6
torinoutacnh <54936986+torinoutacnh@users.noreply.github.com>
2019-11-29 06:25:32 +0700

    Update US_dkPTN.cs

--- Visual/DoAn/QuanLy_DoAn_TNTH/QuanLy_DoAn_TNTH/US_dkPTN.cs
+++ Visual/DoAn/QuanLy_DoAn_TNTH/QuanLy_DoAn_TNTH/US_dkPTN.cs
@@ -84,14 +84,82 @@ namespace QuanLy_DoAn_TNTH
             adap.Fill(dt);            
             dataGridView_ThietBi.DataSource = dt;
 
+            cm = new SqlCommand("select * from ThietBi", sql);
+            adap = new SqlDataAdapter(cm);
+            dt = new DataTable();
+            adap.Fill(dt);
+            dataGridView_ThietBi.DataSource = dt;
+
+            cm = new SqlCommand("select * from DK_PTN", sql);
+            adap = new SqlDataAdapter(cm);
+            dt = new DataTable();
+            adap.Fill(dt);
+            dataGridView2.DataSource = dt;
+            sql.Close();
+
             txtMaTB.Enabled = false;
 
             sql.Close();
         }
 
+        public Boolean exedata(string cmd)
+        {
+            SqlConnection sql = DBUtils.GetDBConnection();
+            sql.Open();
+            Boolean check = false;
+            try
+            {
+                SqlCommand sc = new SqlCommand(cmd, sql);
+                sc.ExecuteNonQuery();
+                check = true;
+            }
+            catch (Exception)
+            {
+                check = false;
+            }
+            sql.Close();
+            return check;
+        }
         private void btnThem_Click(object sender, EventArgs e)
         {
+            if(txtMaTB.Text.Trim() == "")
+            {
+                MessageBox.Show("Chọn Thiết Bị !");
+                return;
+            }
+            if (txtSoLuong.Text.Trim() == "")
+            {
+                MessageBox.Show("Nhập số Lượng !");
+                return;
+            }
+            if (date.Value <= DateTime.Now)
+            {
+                MessageBox.Show("Nhập Ngày Khác !");
+                return;
+            }
+            string nhom=cbTenNhom.SelectedValue.ToString().Trim(); ;
+            string loai= cbLoaiTN.SelectedValue.ToString().Trim(); ;
+            DateTime ngay=date.Value;
+            string gv=cbTenGV.SelectedValue.ToString().Trim(); ;
+            string phong= cbTenPhong.SelectedValue.ToString().Trim(); ;
+            string buoi= cbBuoi.SelectedValue.ToString().Trim(); ;
+            string tb= txtMaTB.Text.Trim();
+            int sl = Int32.Parse(txtSoLuong.Text.Trim());
 
+            if (!exedata($"exec DKPTN '{ngay}','{buoi}','{nhom}','{gv}','{loai}','{phong}','{tb}',{sl};"))
+                MessageBox.Show("Có Lỗi !");
+            else
+                MessageBox.Show("Thành công !");
+
+            //-------------------grid TB
+            SqlConnection sql = DBUtils.GetDBConnection();
+            sql.Open();            
+            SqlCommand cm = new SqlCommand("select * from DK_PTN", sql);
+            SqlDataAdapter adap = new SqlDataAdapter(cm);
+            DataTable dt = new DataTable();
+            adap.Fill(dt);
+            dataGridView2.DataSource = dt;
+            sql.Close();
         }
 
         private void dataGridView_ThietBi_CellClick(object sender, DataGridViewCellEventArgs e)
@@ -99,6 +167,26 @@ namespace QuanLy_DoAn_TNTH
             int numrow;
             numrow = e.RowIndex;
             txtMaTB.Text = dataGridView_ThietBi.Rows[numrow].Cells[0].Value.ToString();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (this.dataGridView2.SelectedRows.Count > 0)
+            {
+                DateTime ngay = DateTime.Parse(this.dataGridView2.Rows[this.dataGridView2.CurrentRow.Index].Cells[0].Value.ToString());
+                if (!exedata($"delete from DK_PTN where NgayDK='{ngay}'"))
+                    MessageBox.Show("Có Lỗi !");
+                else
+                    MessageBox.Show("Thành công !");
+                SqlConnection sql = DBUtils.GetDBConnection();
+                sql.Open();
+                SqlCommand cm = new SqlCommand("select * from DK_PTN", sql);
+                SqlDataAdapter adap = new SqlDataAdapter(cm);
+                DataTable dt = new DataTable();
+                adap.Fill(dt);
+                dataGridView2.DataSource = dt;
+                sql.Close();
+            }
         }
     }
 }
