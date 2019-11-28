3d850fbe22316354381ae2c9328e75e576d67907
torinoutacnh <54936986+torinoutacnh@users.noreply.github.com>
2019-11-29 00:12:16 +0700

    Update US_dkPTN.cs

--- Visual/DoAn/QuanLy_DoAn_TNTH/QuanLy_DoAn_TNTH/US_dkPTN.cs
+++ Visual/DoAn/QuanLy_DoAn_TNTH/QuanLy_DoAn_TNTH/US_dkPTN.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -28,20 +28,77 @@ namespace QuanLy_DoAn_TNTH
             }
         }
 
-        private void btTimKiem_Click(object sender, EventArgs e)
+        private void groupBox1_Enter(object sender, EventArgs e)
         {
-            SqlConnection sql = DBUtils.GetDBConnection();
-            sql.Open();
-            SqlCommand cmd = new SqlCommand("Select * from ThietBi where TenTB LIKE '%"+txtTenTB.Text+"%'", sql);
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            da.Fill(dt);
-            dataGridView_ThietBi.DataSource = dt;
+
         }
 
-        private void dataGridView_ThietBi_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void US_dkPTN_Load(object sender, EventArgs e)
         {
-            txtMaTB.Text = dataGridView_ThietBi[0, e.RowIndex].Value.ToString();
+            //-----------load nhom
+            SqlConnection sql = DBUtils.GetDBConnection();
+            sql.Open();
+            SqlCommand cm = new SqlCommand("select MaNhom,TenNhom from NhomSV", sql);
+            SqlDataAdapter adap = new SqlDataAdapter(cm);
+            DataTable dt = new DataTable();
+            adap.Fill(dt);
+            cbTenNhom.DisplayMember = "TenNhom";
+            cbTenNhom.ValueMember = "MaNhom";
+            cbTenNhom.DataSource = dt;
+            //-----------load loại TN
+            cm = new SqlCommand("select MaLoaiTN,TenLoai from Loai_TN", sql);
+            adap = new SqlDataAdapter(cm);
+            dt = new DataTable();
+            adap.Fill(dt);
+            cbLoaiTN.DisplayMember = "TenLoai";
+            cbLoaiTN.ValueMember = "MaLoaiTN";
+            cbLoaiTN.DataSource = dt;
+            //-----------load loại TN
+            cm = new SqlCommand("select MaPTN,TenPTN from PhongThiNghiem", sql);
+            adap = new SqlDataAdapter(cm);
+            dt = new DataTable();
+            adap.Fill(dt);
+            cbTenPhong.DisplayMember = "TenPTN";
+            cbTenPhong.ValueMember = "MaPTN";
+            cbTenPhong.DataSource = dt;
+            //-------------------GVQL
+            cm = new SqlCommand("select MaGVQL,TenGVQL from GV_QLTN", sql);
+            adap = new SqlDataAdapter(cm);
+            dt = new DataTable();
+            adap.Fill(dt);
+            cbTenGV.DisplayMember = "TenGVQL";
+            cbTenGV.ValueMember = "MaGVQL";
+            cbTenGV.DataSource = dt;
+            //---------------------Buổi
+            cm = new SqlCommand("select MaBuoi,Tiet from Buoi", sql);
+            adap = new SqlDataAdapter(cm);
+            dt = new DataTable();
+            adap.Fill(dt);
+            cbBuoi.DisplayMember = "Tiet";
+            cbBuoi.ValueMember = "MaBuoi";
+            cbBuoi.DataSource = dt;
+            //-------------------grid TB
+            cm = new SqlCommand("select MaTB,TenTB from ThietBi", sql);
+            adap = new SqlDataAdapter(cm);
+            dt = new DataTable();
+            adap.Fill(dt);            
+            dataGridView_ThietBi.DataSource = dt;
+
+            txtMaTB.Enabled = false;
+
+            sql.Close();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView_ThietBi_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int numrow;
+            numrow = e.RowIndex;
+            txtMaTB.Text = dataGridView_ThietBi.Rows[numrow].Cells[0].Value.ToString();
         }
     }
 }
