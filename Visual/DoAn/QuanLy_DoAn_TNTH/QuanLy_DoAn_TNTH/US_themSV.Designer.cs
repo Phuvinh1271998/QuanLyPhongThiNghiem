cb0e0d11ace90c1c14bed95dadfc246e9313f3d3
torinoutacnh <54936986+torinoutacnh@users.noreply.github.com>
2019-11-29 02:42:03 +0700

    Update US_themSV.Designer.cs

--- Visual/DoAn/QuanLy_DoAn_TNTH/QuanLy_DoAn_TNTH/US_themSV.Designer.cs
+++ Visual/DoAn/QuanLy_DoAn_TNTH/QuanLy_DoAn_TNTH/US_themSV.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace QuanLy_DoAn_TNTH
+namespace QuanLy_DoAn_TNTH
 {
     partial class US_themSV
     {
@@ -35,27 +35,39 @@
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
-            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
-            this.textEdit5 = new DevExpress.XtraEditors.TextEdit();
-            this.textEdit6 = new DevExpress.XtraEditors.TextEdit();
-            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
-            this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
-            this.labelControl7 = new DevExpress.XtraEditors.LabelControl();
+            this.cbTenNhom = new System.Windows.Forms.ComboBox();
+            this.cbLop = new System.Windows.Forms.ComboBox();
+            this.txtNamHoc = new System.Windows.Forms.TextBox();
+            this.txtTenSV = new System.Windows.Forms.TextBox();
+            this.txtHocKy = new System.Windows.Forms.TextBox();
+            this.txtMaSV = new System.Windows.Forms.TextBox();
             this.simpleButton6 = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl7 = new DevExpress.XtraEditors.LabelControl();
             this.simpleButton4 = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
             this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
             this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
             this.simpleButton5 = new DevExpress.XtraEditors.SimpleButton();
+            this.textEdit6 = new DevExpress.XtraEditors.TextEdit();
+            this.textEdit5 = new DevExpress.XtraEditors.TextEdit();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnIn = new System.Windows.Forms.Button();
+            this.dateNgaySinh = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit3.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit4.Properties)).BeginInit();
             this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit5.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit6.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit5.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -105,6 +117,18 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.dateNgaySinh);
+            this.groupBox2.Controls.Add(this.btnIn);
+            this.groupBox2.Controls.Add(this.btnLuu);
+            this.groupBox2.Controls.Add(this.btnSua);
+            this.groupBox2.Controls.Add(this.btnXoa);
+            this.groupBox2.Controls.Add(this.btnThem);
+            this.groupBox2.Controls.Add(this.cbTenNhom);
+            this.groupBox2.Controls.Add(this.cbLop);
+            this.groupBox2.Controls.Add(this.txtNamHoc);
+            this.groupBox2.Controls.Add(this.txtTenSV);
+            this.groupBox2.Controls.Add(this.txtHocKy);
+            this.groupBox2.Controls.Add(this.txtMaSV);
             this.groupBox2.Controls.Add(this.simpleButton6);
             this.groupBox2.Controls.Add(this.labelControl7);
             this.groupBox2.Controls.Add(this.simpleButton4);
@@ -130,69 +154,51 @@
             this.groupBox2.TabIndex = 7;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Thêm sinh viên";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
             // 
-            // labelControl3
+            // cbTenNhom
             // 
-            this.labelControl3.Location = new System.Drawing.Point(17, 118);
-            this.labelControl3.Name = "labelControl3";
-            this.labelControl3.Size = new System.Drawing.Size(32, 13);
-            this.labelControl3.TabIndex = 6;
-            this.labelControl3.Text = "Học kỳ";
+            this.cbTenNhom.FormattingEnabled = true;
+            this.cbTenNhom.Location = new System.Drawing.Point(410, 71);
+            this.cbTenNhom.Name = "cbTenNhom";
+            this.cbTenNhom.Size = new System.Drawing.Size(121, 21);
+            this.cbTenNhom.TabIndex = 34;
             // 
-            // labelControl4
+            // cbLop
             // 
-            this.labelControl4.Location = new System.Drawing.Point(338, 31);
-            this.labelControl4.Name = "labelControl4";
-            this.labelControl4.Size = new System.Drawing.Size(47, 13);
-            this.labelControl4.TabIndex = 7;
-            this.labelControl4.Text = "Ngày sinh";
+            this.cbLop.FormattingEnabled = true;
+            this.cbLop.Location = new System.Drawing.Point(94, 158);
+            this.cbLop.Name = "cbLop";
+            this.cbLop.Size = new System.Drawing.Size(121, 21);
+            this.cbLop.TabIndex = 33;
             // 
-            // maskedTextBox1
+            // txtNamHoc
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(410, 28);
-            this.maskedTextBox1.Mask = "00/00/0000";
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(139, 20);
-            this.maskedTextBox1.TabIndex = 8;
-            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            this.txtNamHoc.Location = new System.Drawing.Point(410, 114);
+            this.txtNamHoc.Name = "txtNamHoc";
+            this.txtNamHoc.Size = new System.Drawing.Size(100, 20);
+            this.txtNamHoc.TabIndex = 32;
             // 
-            // textEdit5
+            // txtTenSV
             // 
-            this.textEdit5.Location = new System.Drawing.Point(93, 160);
-            this.textEdit5.Name = "textEdit5";
-            this.textEdit5.Size = new System.Drawing.Size(139, 20);
-            this.textEdit5.TabIndex = 9;
+            this.txtTenSV.Location = new System.Drawing.Point(94, 71);
+            this.txtTenSV.Name = "txtTenSV";
+            this.txtTenSV.Size = new System.Drawing.Size(100, 20);
+            this.txtTenSV.TabIndex = 29;
             // 
-            // textEdit6
+            // txtHocKy
             // 
-            this.textEdit6.Location = new System.Drawing.Point(410, 71);
-            this.textEdit6.Name = "textEdit6";
-            this.textEdit6.Size = new System.Drawing.Size(139, 20);
-            this.textEdit6.TabIndex = 10;
+            this.txtHocKy.Location = new System.Drawing.Point(94, 114);
+            this.txtHocKy.Name = "txtHocKy";
+            this.txtHocKy.Size = new System.Drawing.Size(100, 20);
+            this.txtHocKy.TabIndex = 28;
             // 
-            // labelControl5
+            // txtMaSV
             // 
-            this.labelControl5.Location = new System.Drawing.Point(338, 74);
-            this.labelControl5.Name = "labelControl5";
-            this.labelControl5.Size = new System.Drawing.Size(43, 13);
-            this.labelControl5.TabIndex = 11;
-            this.labelControl5.Text = "Mã nhóm";
-            // 
-            // labelControl6
-            // 
-            this.labelControl6.Location = new System.Drawing.Point(338, 118);
-            this.labelControl6.Name = "labelControl6";
-            this.labelControl6.Size = new System.Drawing.Size(41, 13);
-            this.labelControl6.TabIndex = 12;
-            this.labelControl6.Text = "Năm học";
-            // 
-            // labelControl7
-            // 
-            this.labelControl7.Location = new System.Drawing.Point(17, 163);
-            this.labelControl7.Name = "labelControl7";
-            this.labelControl7.Size = new System.Drawing.Size(17, 13);
-            this.labelControl7.TabIndex = 13;
-            this.labelControl7.Text = "Lớp";
+            this.txtMaSV.Location = new System.Drawing.Point(94, 28);
+            this.txtMaSV.Name = "txtMaSV";
+            this.txtMaSV.Size = new System.Drawing.Size(100, 20);
+            this.txtMaSV.TabIndex = 9;
             // 
             // simpleButton6
             // 
@@ -202,6 +208,14 @@
             this.simpleButton6.TabIndex = 26;
             this.simpleButton6.Text = "In";
             // 
+            // labelControl7
+            // 
+            this.labelControl7.Location = new System.Drawing.Point(17, 163);
+            this.labelControl7.Name = "labelControl7";
+            this.labelControl7.Size = new System.Drawing.Size(17, 13);
+            this.labelControl7.TabIndex = 13;
+            this.labelControl7.Text = "Lớp";
+            // 
             // simpleButton4
             // 
             this.simpleButton4.Location = new System.Drawing.Point(315, 211);
@@ -209,6 +223,14 @@
             this.simpleButton4.Size = new System.Drawing.Size(80, 30);
             this.simpleButton4.TabIndex = 25;
             this.simpleButton4.Text = "Lưu";
+            // 
+            // labelControl6
+            // 
+            this.labelControl6.Location = new System.Drawing.Point(338, 118);
+            this.labelControl6.Name = "labelControl6";
+            this.labelControl6.Size = new System.Drawing.Size(41, 13);
+            this.labelControl6.TabIndex = 12;
+            this.labelControl6.Text = "Năm học";
             // 
             // simpleButton3
             // 
@@ -226,6 +248,14 @@
             this.simpleButton2.TabIndex = 23;
             this.simpleButton2.Text = "Xóa";
             // 
+            // labelControl5
+            // 
+            this.labelControl5.Location = new System.Drawing.Point(338, 74);
+            this.labelControl5.Name = "labelControl5";
+            this.labelControl5.Size = new System.Drawing.Size(47, 13);
+            this.labelControl5.TabIndex = 11;
+            this.labelControl5.Text = "Tên nhóm";
+            // 
             // simpleButton5
             // 
             this.simpleButton5.Location = new System.Drawing.Point(22, 211);
@@ -233,6 +263,45 @@
             this.simpleButton5.Size = new System.Drawing.Size(80, 30);
             this.simpleButton5.TabIndex = 22;
             this.simpleButton5.Text = "Thêm";
+            // 
+            // textEdit6
+            // 
+            this.textEdit6.Location = new System.Drawing.Point(410, 71);
+            this.textEdit6.Name = "textEdit6";
+            this.textEdit6.Size = new System.Drawing.Size(139, 20);
+            this.textEdit6.TabIndex = 10;
+            // 
+            // textEdit5
+            // 
+            this.textEdit5.Location = new System.Drawing.Point(93, 160);
+            this.textEdit5.Name = "textEdit5";
+            this.textEdit5.Size = new System.Drawing.Size(139, 20);
+            this.textEdit5.TabIndex = 9;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(410, 28);
+            this.maskedTextBox1.Mask = "00/00/0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(139, 20);
+            this.maskedTextBox1.TabIndex = 8;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Location = new System.Drawing.Point(338, 31);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(47, 13);
+            this.labelControl4.TabIndex = 7;
+            this.labelControl4.Text = "Ngày sinh";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(17, 118);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(32, 13);
+            this.labelControl3.TabIndex = 6;
+            this.labelControl3.Text = "Học kỳ";
             // 
             // dataGridView1
             // 
@@ -242,6 +311,61 @@
             this.dataGridView1.Size = new System.Drawing.Size(593, 150);
             this.dataGridView1.TabIndex = 8;
             // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(25, 214);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 35;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(119, 214);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 36;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(221, 214);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 37;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(319, 214);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(75, 23);
+            this.btnLuu.TabIndex = 38;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            // 
+            // btnIn
+            // 
+            this.btnIn.Location = new System.Drawing.Point(410, 214);
+            this.btnIn.Name = "btnIn";
+            this.btnIn.Size = new System.Drawing.Size(75, 23);
+            this.btnIn.TabIndex = 39;
+            this.btnIn.Text = "In";
+            this.btnIn.UseVisualStyleBackColor = true;
+            // 
+            // dateNgaySinh
+            // 
+            this.dateNgaySinh.Location = new System.Drawing.Point(410, 28);
+            this.dateNgaySinh.Name = "dateNgaySinh";
+            this.dateNgaySinh.Size = new System.Drawing.Size(171, 20);
+            this.dateNgaySinh.TabIndex = 40;
+            // 
             // US_themSV
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -250,14 +374,15 @@
             this.Controls.Add(this.groupBox2);
             this.Name = "US_themSV";
             this.Size = new System.Drawing.Size(602, 445);
+            this.Load += new System.EventHandler(this.US_themSV_Load);
             ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit3.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit4.Properties)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit5.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit6.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit5.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -286,5 +411,17 @@
         private DevExpress.XtraEditors.SimpleButton simpleButton2;
         private DevExpress.XtraEditors.SimpleButton simpleButton5;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cbTenNhom;
+        private System.Windows.Forms.ComboBox cbLop;
+        private System.Windows.Forms.TextBox txtNamHoc;
+        private System.Windows.Forms.TextBox txtTenSV;
+        private System.Windows.Forms.TextBox txtHocKy;
+        private System.Windows.Forms.TextBox txtMaSV;
+        private System.Windows.Forms.Button btnIn;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.DateTimePicker dateNgaySinh;
     }
 }
